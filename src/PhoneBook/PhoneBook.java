16608b7ed1ab8bd02c39d22ce985839bfe4c7ca4
1 <Golzitskynikolay@gmail.com>
2018-03-10 18:37:33 +0300

    first project

--- src/PhoneBook/PhoneBook.java
+++ src/PhoneBook/PhoneBook.java
@@ -2,31 +2,42 @@ package PhoneBook;
 
 import java.util.*;
 
-
 public class PhoneBook {
 
     private Map<String, String> phoneBookByNum = new HashMap<>();
 
     /**
+     * Create a new contact or add a number to the contact.
      * @param person - name of a contact.
      * @param number - number of a contact.
-     * @throws IllegalArgumentException if the format of the number isn't correct.
-     * Create a new contact or add a number to the contact.
      */
-    public void newContact(String person, String number) {
-        if (!phoneBookByNum.containsKey(number)) {
-            if (!number.matches("(\\+7|8)-((\\d|\\*)|(\\d|\\*){2}|(\\d|\\*){3})" +
-                    "(-((\\d|\\*)|(\\d|\\*){2}|(\\d|\\*){3}))?#")) {
-                throw new IllegalArgumentException("Неверный формат номера!");
-            }
-            phoneBookByNum.put(number, person);
-        } else System.out.println("Такой номер уже есть у другого контакта!");
+    public void newContactOrAddNumber(String person, String number) {
+        if (testOfNumber(number) && personIsCorrect(person)) phoneBookByNum.put(number, person);
+    }
+
+    /**
+     * The number must match the following pattern:
+     * "+7" or "8" then "-". Then can be "*" or digit. At the end of the number must be "#".
+     * After each 3 symbols must be "-", except for the case where after 3 symbols is inserted "#".
+     * The number must contain at least 1 digit or "*".
+     * For example: +7-2*1-345# or 8-111-7# or 8-9#.
+     * @return true, if the number is correct and not null.
+     * @throws IllegalArgumentException if the format of the number isn't correct.
+     */
+    private boolean testOfNumber(String number) {
+        if (numberIsCorrect(number) && phoneBookByNum.containsKey(number)) {
+            throw new IllegalArgumentException("Такой номер уже есть у другого контакта!");
+        }
+        if (!number.matches("(\\+7|8)-((\\d|\\*)|(\\d|\\*){2}|(\\d|\\*){3})" +
+                "(-((\\d|\\*)|(\\d|\\*){2}|(\\d|\\*){3}))?#")) {
+            throw new IllegalArgumentException("Неверный формат номера!");
+        }
+        return true;
     }
 
     /**
      * Delete the number of your contact.
      */
-
     public void deleteNumber(String number) {
         phoneBookByNum.remove(number);
     }
@@ -34,7 +45,6 @@ public class PhoneBook {
     /**
      * Delete your contact.
      */
-
     public void deletePerson(String person) {
         phoneBookByNum.entrySet().removeIf(entry -> entry.getValue().equals(person));
     }
@@ -44,9 +54,12 @@ public class PhoneBook {
      *
      * @return name of your contact.
      */
-
     public String findPerson(String number) {
-        return phoneBookByNum.get(number);
+        String result = null;
+        if (numberIsCorrect(number)) {
+            result = phoneBookByNum.get(number);
+        }
+        return result;
     }
 
     /**
@@ -54,19 +67,36 @@ public class PhoneBook {
      *
      * @return number(s) of your contact.
      */
-
     public ArrayList<String> findNumber(String person) {
         ArrayList<String> numbers = new ArrayList<>();
         phoneBookByNum.entrySet().stream().filter(e -> e.getValue().
                 equals(person)).forEach(e -> numbers.add(e.getKey()));
-        Collections.sort(numbers);
         return numbers;
+    }
+
+    /**
+     * @param number can't have a null.
+     * @return true, if number is a correct.
+     */
+    private boolean numberIsCorrect(String number) {
+        if (number == null || number.matches(" *"))
+            throw new NullPointerException("Номер введён не корректно!");
+        return true;
+    }
+
+    /**
+     * @param person can't have a null.
+     * @return true, if person is a correct.
+     */
+    private boolean personIsCorrect(String person) {
+        if (person == null || person.matches(" *"))
+            throw new NullPointerException("Имя контакта введено не корректно!");
+        return true;
     }
 
     /**
      * @return a string containing numbers and contact names.
      */
-
     @Override
     public String toString() {
         return phoneBookByNum.toString();
--- test/PhoneBook/PhoneBookTest.java
+++ test/PhoneBook/PhoneBookTest.java
@@ -6,34 +6,112 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PhoneBookTest {
     @Test
-    public void newContact() {
+    public void newContactOrAddNumber() {
         PhoneBook book = new PhoneBook();
-        book.newContact("Вася", "+7-12#");
-        book.newContact("Тепя", "8-523#");
-        book.newContact("Sema", "8-5*3-1#");
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        book.newContactOrAddNumber("Тепя", "8-523#");
+        book.newContactOrAddNumber("Sema", "8-5*3-1#");
         assertEquals("{8-523#=Тепя, 8-5*3-1#=Sema, +7-12#=Вася}", book.toString());
         try {
-            book.newContact("Лена", "8-5---*3-1#");
+            book.newContactOrAddNumber("", "8-5*3#");
             fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Имя контакта введено не корректно!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void numberIsCorrect() {
+        PhoneBook book = new PhoneBook();
+        try {
+            book.newContactOrAddNumber("MAMA", null);
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Номер введён не корректно!", e.getMessage());
+        }
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        assertTrue(true);
+        try {
+            book.newContactOrAddNumber("MAMA2", "");
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Номер введён не корректно!", e.getMessage());
+        }
+        try {
+            book.newContactOrAddNumber("MAMA", "     ");
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Номер введён не корректно!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void personIsCorrect() {
+        PhoneBook book = new PhoneBook();
+        try {
+            book.newContactOrAddNumber(null, "8-12#");
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Имя контакта введено не корректно!", e.getMessage());
+        }
+        book.newContactOrAddNumber("Вася 3", "+7-142#");
+        assertTrue(true);
+        try {
+            book.newContactOrAddNumber("", "8-15#");
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Имя контакта введено не корректно!", e.getMessage());
+        }
+        try {
+            book.newContactOrAddNumber(" ", "8-14#");
+            fail("Exception expected");
+        } catch (NullPointerException e) {
+            assertEquals("Имя контакта введено не корректно!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testOfNumber() {
+        PhoneBook book = new PhoneBook();
+        try {
+            book.newContactOrAddNumber("Alena", "8-5---*3-1#");
+            fail("Exception 1 expected");
         } catch (IllegalArgumentException e) {
             assertEquals("Неверный формат номера!", e.getMessage());
+        }
+        book.newContactOrAddNumber("Ilia", "8-57#");
+        assertTrue(true);
+        try {
+            book.newContactOrAddNumber("Seva", "8-57#");
+            fail("Exception 2 expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Такой номер уже есть у другого контакта!", e.getMessage());
+        }
+        try {
+            book.newContactOrAddNumber("Vasya", null);
+            fail("Exception 3 expected");
+        } catch (NullPointerException e) {
+            assertEquals("Номер введён не корректно!", e.getMessage());
         }
     }
 
     @Test
     public void deleteNumber() {
         PhoneBook book = new PhoneBook();
-        book.newContact("Вася", "+7-12#");
-        book.newContact("Тепя", "8-523#");
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        book.newContactOrAddNumber("Тепя", "8-523#");
         book.deleteNumber("+7-12#");
         assertEquals("{8-523#=Тепя}", book.toString());
+        book.deleteNumber(null);
+        assertEquals("{8-523#=Тепя}", book.toString());
+
     }
 
     @Test
     public void deletePerson() {
         PhoneBook book = new PhoneBook();
-        book.newContact("Вася", "+7-12#");
-        book.newContact("Тепя", "8-523#");
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        book.newContactOrAddNumber("Тепя", "8-523#");
         book.deletePerson("Вася");
         assertEquals("{8-523#=Тепя}", book.toString());
     }
@@ -41,22 +119,23 @@ public class PhoneBookTest {
     @Test
     public void findPerson() {
         PhoneBook book = new PhoneBook();
-        book.newContact("Вася", "+7-12#");
-        book.newContact("Тепя", "8-523#");
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        book.newContactOrAddNumber("Тепя", "8-523#");
         assertEquals("Тепя", book.findPerson("8-523#"));
         assertEquals("Вася", book.findPerson("+7-12#"));
         assertEquals(null, book.findPerson("8-8#"));
+
     }
 
 
     @Test
     public void findNumber() {
         PhoneBook book = new PhoneBook();
-        book.newContact("Вася", "+7-12#");
-        book.newContact("Вася", "+7-123#");
-        book.newContact("Вася", "8-*64-5#");
-        book.newContact("Тепя", "+7-1#");
+        book.newContactOrAddNumber("Вася", "+7-12#");
+        book.newContactOrAddNumber("Вася", "+7-123#");
+        book.newContactOrAddNumber("Вася", "8-*64-5#");
+        book.newContactOrAddNumber("Тепя", "+7-1#");
         assertArrayEquals(new String[]{"+7-1#"}, book.findNumber("Тепя").toArray());
-        assertArrayEquals(new String[]{"+7-12#", "+7-123#", "8-*64-5#"}, book.findNumber("Вася").toArray());
+        assertArrayEquals(new String[]{"+7-123#", "+7-12#", "8-*64-5#"}, book.findNumber("Вася").toArray());
     }
 }
